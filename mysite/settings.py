5621a2d73c7f9022a38ac52314285a6e65b2af41
石村 雅宏 <ISHIMURA-M@i-cube.local>
2023-08-01 21:18:47 +0900

    5

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
 }
 
 
