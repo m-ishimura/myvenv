386551fb66e9ab4cae94e439d0eadce870c35a4f
石村 雅宏 <ISHIMURA-M@i-cube.local>
2023-08-07 18:03:55 +0900

    2023-08-07 5

--- polls/models.py
+++ polls/models.py
@@ -3,18 +3,30 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def __str__(self):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 class Choice(models.Model):
